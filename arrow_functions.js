d4613a0ab4a9f40e17913019af4352e995b8ba47
Tumilara Oresanya <oresanyatumilara@gmail.com>
2025-09-27 07:57:14 -0700

    Add files via upload

---
+++ arrow_functions.js
@@ -0,0 +1,7 @@
+// an example on arrow functions in JavaScript
+const add = (a, b) => a + b;
+
+const greet = (name) => `Hello, ${name}!`;
+
+console.log(add(2, 3));
+console.log(greet("Tumilara"));
